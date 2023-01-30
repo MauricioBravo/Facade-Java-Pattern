71874d08bb0d0eea9f97c4afa1a0ed86f1a4b6ea
MauricioBravo <qwe455r@gmail.com>
2023-01-30 19:43:27 -0300

    facade with  singleton

--- src/App/App.java
+++ src/App/App.java
@@ -6,10 +6,10 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		ServiceMaker cliente1 = new ServiceMaker();
+		ServiceMaker cliente1 = ServiceMaker.getInstancia();
 		cliente1.buscar("02/07/2023","08/07/2023", "Mendoza","Potrerillos");
 		
-		ServiceMaker cliente2 = new ServiceMaker();
+		ServiceMaker cliente2 = ServiceMaker.getInstancia();
 		cliente2.buscar("02/05/2023","08/05/2023", "Mendoza","San Rafael");
 
 		
--- src/facade/ServiceMaker.java
+++ src/facade/ServiceMaker.java
@@ -7,16 +7,28 @@ import api.Hotel;
 
 public class ServiceMaker {
 
-	
+		private static ServiceMaker instancia;
 		private IntServicio hotel;
 		private IntServicio avion;
 		
-		public ServiceMaker() {
+		private ServiceMaker() {
 			hotel = new Hotel();
 			avion = new Avion();
 		}
 		
-		public void buscar(String fechaIda, String fechaVuelta, String origen, String destino) {
+		public static ServiceMaker getInstancia() {
+			if (instancia ==null) {
+				instancia = new ServiceMaker();
+				
+			}
+			return instancia;
+		}
+		
+		
+		
+		public void buscar(String fechaIda, String fechaVuelta,
+			String origen, String destino) {
+			
 			hotel.buscar(fechaIda,fechaVuelta, origen,destino);
 			avion.buscar(fechaIda,fechaVuelta, origen,destino);
 			System.out.println(hotel instanceof IntServicio);
