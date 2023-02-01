c6fb2a5e9270118bad5c0e1e5a03f76adf6a65df
MauricioBravo <qwe455r@gmail.com>
2023-01-31 23:27:53 -0300

    multiples imput funcionan perfecto

--- src/App/App.java
+++ src/App/App.java
@@ -8,26 +8,23 @@ public class App {
 	public static void main(String[] args) {
 
 		final String provincia;
-		final String localidad ="null";
-		final String fechaSalida =" null";
-		final String fechaVuelta = "null";
-		
-
-		System.out.println("ingrese la provincia destino");	
+		final String localidad;
+		final String fechaSalida;
+		final String fechaVuelta;
 
 		try (Scanner myObj = new Scanner(System.in)) {
+			System.out.println("ingrese la provincia destino");	
 			provincia = myObj.nextLine();
+			System.out.println("ingrese la localidad destino");	
+			localidad = myObj.nextLine();
+			System.out.println("ingrese la fecha de salida");	
+			fechaSalida = myObj.nextLine();
+			System.out.println("ingrese la fecha de regreso");	
+			fechaVuelta = myObj.nextLine();
 		}
-
 		
-
-
-		ServiceMaker cliente1 = ServiceMaker.getInstancia();
+			ServiceMaker cliente1 = ServiceMaker.getInstancia();
 		cliente1.buscar(fechaSalida,fechaVuelta, provincia,localidad);
-
-		
-
-
 	}
 
 }
