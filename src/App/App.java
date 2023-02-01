d4ab220faaefc3b61cec5e308931ed0d77880411
MauricioBravo <qwe455r@gmail.com>
2023-01-31 22:33:46 -0300

    destino funcionando

--- src/App/App.java
+++ src/App/App.java
@@ -1,18 +1,33 @@
 package App;
 
 import facade.ServiceMaker;
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
+
+		final String provincia;
+		final String localidad ="null";
+		final String fechaSalida =" null";
+		final String fechaVuelta = "null";
 		
-		ServiceMaker cliente1 = ServiceMaker.getInstancia();
-		cliente1.buscar("02/07/2023","08/07/2023", "Mendoza","Potrerillos");
-		
-		ServiceMaker cliente2 = ServiceMaker.getInstancia();
-		cliente2.buscar("02/05/2023","08/05/2023", "Mendoza","San Rafael");
+
+		System.out.println("ingrese la provincia destino");	
+
+		try (Scanner myObj = new Scanner(System.in)) {
+			provincia = myObj.nextLine();
+		}
 
 		
+
+
+		ServiceMaker cliente1 = ServiceMaker.getInstancia();
+		cliente1.buscar(fechaSalida,fechaVuelta, provincia,localidad);
+
+		
+
+
 	}
 
 }
